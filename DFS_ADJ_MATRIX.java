7c6c067c9cf9c52edcaf5af65ebe2b02624739c2
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2025-03-27 21:46:38 +0530

    Update DFS_ADJ_MATRIX.java

--- DFS_ADJ_MATRIX.java
+++ DFS_ADJ_MATRIX.java
@@ -1,38 +1,47 @@
 import java.util.*;
+
 public class DFSAdjMatrix {
-   int v;
-   int adj[][];
-   boolean visted[];
-   DFSAdjMatrix(int v){
-    this.v = v;
-    adj = new int[v][v];
-    visted = new boolean[v];
-    Arrays.fill(visted,false);
-   }
-   public void DFS(int start){
-    visted[start] = true;
-    System.out.print(start+" ");
-        for(int i=0;i<v;i++){
-            if(adj[start][i]==1 && !visted[i]){
-               DFS(i);
+    int v;
+    int adj[][];
+    boolean visited[];
+
+    DFSAdjMatrix(int v) {
+        this.v = v;
+        adj = new int[v][v];
+        visited = new boolean[v];
+        Arrays.fill(visited, false);
+    }
+
+
+    public void DFS(int start) {
+        visited[start] = true;
+        System.out.print(start + " ");
+        for (int i = 0; i < v; i++) {
+            if (adj[start][i] == 1 && !visited[i]) {
+                DFS(i);
             }
         }
     }
 
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("no of vertices:");
-    int v = sc.nextInt();
-    DFSAdjMatrix b = new DFSAdjMatrix(v);
-    System.out.println("enter adj matrix: "+v+'X'+v );
-    for(int i=0;i<v;i++){
-        for(int j=0;j<v;j++){
-            b.adj[i][j]=sc.nextInt();
-        }
-    }
-    System.out.println("enter starting vertex for BFS:");
-    int start= sc.nextInt();
-    b.DFS(start);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("No of vertices: ");
+        int v = sc.nextInt();
 
-   }
+        DFSAdjMatrix b = new DFSAdjMatrix(v); 
+
+        System.out.println("Enter adjacency matrix (" + v + "x" + v + "): ");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                b.adj[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter starting vertex for DFS:");
+        int start = sc.nextInt();
+
+        System.out.println("DFS Traversal:");
+        b.DFS(start);
+        sc.close();
+    }
 }
