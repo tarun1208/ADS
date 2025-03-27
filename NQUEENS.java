bab0bf94e989c7ece07ec9422ab10e8762daa1ab
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2025-03-27 13:08:22 +0530

    Update NQUEENS.java

--- NQUEENS.java
+++ NQUEENS.java
@@ -1,35 +1,40 @@
-import java.util.*;
-public class NQueens {
-    static int[] x;
-   public static boolean isplace(int k, int i) {
+import java.util.Scanner;
+
+public class Nqueens1 {
+    private static int[] x; 
+
+    public static boolean Place(int k, int i) {
         for (int j = 1; j < k; j++) {
             if (x[j] == i || Math.abs(x[j] - i) == Math.abs(j - k)) {
                 return false;
             }
         }
-        return true; 
+        return true;
     }
-   public static void NQueens(int k, int n) {
+
+    public static void nqueens(int k, int n) {
         if (k > n) {
-            for (int i = 1; i <= n; i++)
+            for (int i = 1; i <= n; i++) {
                 System.out.print(x[i] + " ");
+            }
             System.out.println();
-            return;
-        }
-       for (int i = 1; i <= n; i++) {
-            if (isplace(k, i)) {
-                x[k] = i;
-                NQueens(k + 1, n); 
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (Place(k, i)) {
+                    x[k] = i;
+                    nqueens(k + 1, n);
+                }
             }
         }
     }
-   public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of Queens: ");
+        System.out.print("Enter the number of queens: ");
         int n = sc.nextInt();
-        x = new int[n + 1]; 
-        System.out.println("Solutions:");
-        NQueens(1, n);
         sc.close();
+
+        x = new int[n + 1];
+        nqueens(1, n);
     }
 }
